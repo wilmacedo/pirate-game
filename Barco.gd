53e4aa4e66bf0a2239a73045d726218f3653f30e
wilmacedo <wil.macedo.sa@gmail.com>
2021-06-13 16:52:39 -0300

    update scripts refs

--- Barco.gd
+++ Barco.gd
@@ -10,17 +10,9 @@ func _physics_process(delta):
 		"left": Vector2(-speed, 0),
 		"right": Vector2(speed, 0)
 	}
-		
 	
-	if Input.is_action_pressed("down"):
-		direction = Vector2(0, speed)
-	elif Input.is_action_pressed("up"):
-		direction = Vector2(0, -speed)
-	elif Input.is_action_pressed("left"):
-		direction = Vector2(-speed, 0)
-	elif Input.is_action_pressed("right"):
-		direction = Vector2(speed, 0)
-	else:
-		direction = Vector2()
+	for dir in vectors:
+		if Input.is_action_pressed(dir):
+			direction = vectors[dir]
 	
 	move_and_slide(direction)
