bb485e4e065acd524f348f80b55537581f96fc63
JorgeLins <jorgelins1155@gmail.com>
2021-06-14 20:55:01 -0300

    collision

--- Global.gd
+++ Global.gd
@@ -8,16 +8,16 @@ export var defaultSpeed = 10
 
 export var piratesInfo = [
 	{
-		'name': 'Pirate 1',
-		'description': 'Description for pirate 1'
+		'name': 'Barba Negra',
+		'description': 'Possivelmente o pirata mais famoso de todos, Edward Teach era mais conhecido como Barba Negra. A bordo de seu navio principal, o Queen Anne’s Revenge, Barba Negra organizou a captura de muitos navios e aterrorizou o Caribe e as colônias americanas.'
 	},
 	{
-		'name': 'Pirate 2',
-		'description': 'Description for pirate 2'
+		'name': 'Black Bart',
+		'description': 'Este foi o pirata de mais sucesso do seu tempo. Ele assaltou mais de 400 navios e era considerado quase invencível. Forçado a se juntar aos piratas que capturaram seu navio, Roberts revelou ter talento como pirata e se tornou capitão de uma frota.'
 	},
 	{
-		'name': 'Pirate 3',
-		'description': 'Description for pirate 3'
+		'name': 'Calico jack',
+		'description': 'John Rackham, mais conhecido como Calico Jack, foi um famoso pirata inglês no Caribe do século 18. Ele é muito conhecido porque a imagem de sua bandeira pirata (chamada de Jolly Roger) era uma caveira com duas espadas cruzadas, figura que ficou marcada como símbolo dos piratas na cultura popular.'
 	},
 ]
 
