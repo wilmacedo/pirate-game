21c41217fa6d035824e0e762b67747ccd7c5b1a1
wilmacedo <wil.macedo.sa@gmail.com>
2021-06-14 22:39:24 -0300

    add final scene script

--- Global.gd
+++ Global.gd
@@ -5,6 +5,7 @@ export var bau = 0
 export var selectedPirate = 0
 export var atualLocation = 'sea'
 export var defaultSpeed = 10
+export var finish = true
 
 export var piratesInfo = [
 	{
--- Mar.gd
+++ Mar.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
+func finalScene():
+	var boatX = 724.808
+	var boatY = -106.473
+	
+	$Barco.set_position(Vector2(boatX, boatY))
+	
+	$EnemyBoat.set_position(Vector2(529.549, boatY))
+	
+	$EnemyBoat2.set_position(Vector2(901.226, boatY))
+	$EnemyBoat2.scale.x *= -1
+	
+	$EnemyBoat3.set_position(Vector2(boatX, -221.871))
+	$EnemyBoat3.get_node("AnimatedSprite").play('Baixo')
+	
+	$EnemyBoat4.set_position(Vector2(boatX, 7.434))
+	$EnemyBoat4.get_node("AnimatedSprite").play('Cima')
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if Global.finish:
+		finalScene()
+	pass
